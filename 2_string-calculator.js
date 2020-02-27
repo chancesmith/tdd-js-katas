1e5beaa859aa8b3218b21839244fe590bbc584f2
Chance Smith <chancesmithb@gmail.com>
2020-02-26 22:07:41 -0600

    ⚡  IMPROVE: #2 kata - String Calc - refact sum and finding negatives

--- 2_string-calculator.js
+++ 2_string-calculator.js
@@ -9,13 +9,20 @@ class StringCalculator {
     const hasCommaDelimiter = formattedNumString.indexOf(",") > -1;
     if (hasCommaDelimiter) {
       const numbersList = formattedNumString.split(",");
-      return numbersList.reduce((acc, curr) => {
-        if (Number(curr) < 0) throw new Error(`negatives not allowed: -1,-3`);
-        return acc + Number(curr);
-      }, 0);
+      return this.sum(this.foundNegatives(numbersList));
     }
 
     return Number(numString);
+  }
+
+  foundNegatives(numbers) {
+    const negatives = numbers.filter(num => num < 0);
+    if (negatives.length > 0) throw new Error(`negatives not allowed: -1,-3`);
+    return numbers;
+  }
+
+  sum(numbers) {
+    return numbers.reduce((acc, curr) => acc + Number(curr), 0);
   }
 }
 
