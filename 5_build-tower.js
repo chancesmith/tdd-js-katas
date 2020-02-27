0cefb35c8eec5fa73176130caa067324851fa039
Chance Smith <chancesmithb@gmail.com>
2020-02-27 15:13:22 -0600

    📦  NEW: add #5 build tower - WIP

---
+++ 5_build-tower.js
@@ -0,0 +1,9 @@
+// Build Tower by the following given argument:
+// number of floors (integer and always greater than 0).
+// if given 4, return an array of:
+// [
+//   '  *  ',
+//   ' *** ',
+//   ' **** ',
+//   '******',
+// ]
