f1b7d48f7f67314c9e553ff618c7d0d37526eaf7
Chance <chancesmithb@gmail.com>
2020-09-10 10:52:01 -0500

    📦  FEAT: add ListNames kata

---
+++ src/0.1_ListNames/ListNames.test.js
@@ -0,0 +1,13 @@
+// TDD Kata 0.1 - ListNames
+// take an array of employees and return array of names
+// Example: ["Sally", "Tim",...]
+
+const data = require("./employees.js");
+
+describe("ListNames", () => {
+  test("", () => {
+    // first test
+    // console.log({ data });
+    // expect(true).toBe(true);
+  });
+});
---
+++ src/0.1_ListNames/employees.js
@@ -0,0 +1,74 @@
+const employees = [
+  {
+    id: 0,
+    name: "Braden",
+    role: "ADMIN",
+  },
+  {
+    id: 1,
+    name: "Derrick",
+    role: "ADMIN",
+  },
+  {
+    id: 2,
+    name: "Shane",
+    role: "USER",
+  },
+  {
+    id: 3,
+    name: "Emma",
+    role: "USER",
+  },
+  {
+    id: 4,
+    name: "Alex",
+    role: "USER",
+  },
+  {
+    id: 5,
+    name: "Adam",
+    role: "USER",
+  },
+  {
+    id: 6,
+    name: "William",
+    role: "USER",
+  },
+  {
+    id: 7,
+    name: "Jeremy",
+    role: "USER",
+  },
+  {
+    id: 8,
+    name: "Brantley",
+    role: "USER",
+  },
+  {
+    id: 8,
+    name: "Rayyan",
+    role: "ADMIN",
+  },
+  {
+    id: 9,
+    name: "Barrett",
+    role: "ADMIN",
+  },
+  {
+    id: 10,
+    name: "Zach",
+    role: "ADMIN",
+  },
+  {
+    id: 11,
+    name: "Quincy",
+    role: "ADMIN",
+  },
+  {
+    id: 12,
+    name: "Spencer",
+    role: "ADMIN",
+  },
+];
+
+module.exports = employees;
