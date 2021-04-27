f83a52935b096e13912dd2ee64ce481cd0430cac
Chance <chancesmithb@gmail.com>
2021-04-27 08:58:52 -0500

    📦  FEAT: adds garden tests + logic

--- src/7_Gardening/Garden.js
+++ src/7_Gardening/Garden.js
@@ -4,6 +4,23 @@
 // canPlant(garden, 4) --> false // there are only 3 plots, and two of them can't be planted on
 
 class Garden {
+  canPlant(garden=[], plantsCount=0){
+    if(isEmptyGarden(garden)) return false;
+    if(isPlantsEmpty(plantsCount)) return false;
+    if(isEnoughSpotsToPlant(garden, plantsCount)) return true;
+    return false;
+  }
+}
+
+function isEmptyGarden(garden){
+  if (garden.length === 0) return true;
+}
+function isPlantsEmpty(plantsCount){
+  if (plantsCount===0) return true;
+}
+function isEnoughSpotsToPlant(garden, plantsCount){
+  const numOfSpotsLeft = garden.filter(spot => spot === 0).length;
+  if (numOfSpotsLeft >= plantsCount) return true;
 }
 
 module.exports = Garden;
--- src/7_Gardening/Garden.test.js
+++ src/7_Gardening/Garden.test.js
@@ -1,4 +1,34 @@
 const Garden = require("./Garden");
 
 describe("Garden", () => {
+  it('should not plant on empty garden', () => {
+    const g = new Garden()
+    const result = g.canPlant()
+    expect(result).toBe(false)
+  });
+  it('should not plant if no spot is selected', () => {
+    const g = new Garden()
+    const result = g.canPlant([0])
+    expect(result).toBe(false)
+  });
+  it('should plant at position 0', () => {
+    const g = new Garden()
+    const result = g.canPlant([0], 1)
+    expect(result).toBe(true)
+  });
+  it('should not plant at position 0', () => {
+    const g = new Garden()
+    const result = g.canPlant([1], 0)
+    expect(result).toBe(false)
+  });
+  it('should plant at position 2', () => {
+    const g = new Garden()
+    const result = g.canPlant([1,0,0,0,1], 2)
+    expect(result).toBe(true)
+  });
+  it('should not plant two of them', () => {
+    const g = new Garden()
+    const result = g.canPlant([1,0,0,0,1], 4)
+    expect(result).toBe(false)
+  });
 });
