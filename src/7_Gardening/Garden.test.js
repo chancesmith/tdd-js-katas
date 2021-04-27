5b84bc08069cb4a1536f053176d7d2c53cd2c402
Chance <chancesmithb@gmail.com>
2021-04-27 10:45:19 -0500

    ⚡  REFACTOR: [garden] refactor and fix test name

--- src/7_Gardening/Garden.test.js
+++ src/7_Gardening/Garden.test.js
@@ -1,34 +1,28 @@
-const Garden = require("./Garden");
+const canPlant = require("./Garden");
 
 describe("Garden", () => {
   it('should not plant on empty garden', () => {
-    const g = new Garden()
-    const result = g.canPlant()
+    const result = canPlant()
     expect(result).toBe(false)
   });
-  it('should not plant if no spot is selected', () => {
-    const g = new Garden()
-    const result = g.canPlant([0])
+  it('should not plant if no plants given', () => {
+    const result = canPlant([0])
     expect(result).toBe(false)
   });
   it('should plant at position 0', () => {
-    const g = new Garden()
-    const result = g.canPlant([0], 1)
+    const result = canPlant([0], 1)
     expect(result).toBe(true)
   });
   it('should not plant at position 0', () => {
-    const g = new Garden()
-    const result = g.canPlant([1], 0)
+    const result = canPlant([1], 0)
     expect(result).toBe(false)
   });
   it('should plant at position 2', () => {
-    const g = new Garden()
-    const result = g.canPlant([1,0,0,0,1], 2)
+    const result = canPlant([1,0,0,0,1], 2)
     expect(result).toBe(true)
   });
   it('should not plant two of them', () => {
-    const g = new Garden()
-    const result = g.canPlant([1,0,0,0,1], 4)
+    const result = canPlant([1,0,0,0,1], 4)
     expect(result).toBe(false)
   });
 });
