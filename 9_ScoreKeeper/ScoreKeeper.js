83415e50dc8a2746f9732f1d475628f9b821a322
Chance <chancesmithb@gmail.com>
2021-04-29 08:53:15 -0500

    📦  FEAT: finish scorekeeper kata

--- 9_ScoreKeeper/ScoreKeeper.js
+++ 9_ScoreKeeper/ScoreKeeper.js
@@ -1,11 +1,42 @@
-// Your task
-// Write a program that prints one line for each number from 1 to 100
-// For multiples of three print Fizz instead of the number
-// For the multiples of five print Buzz instead of the number
-// For numbers which are multiples of both three and five print FizzBuzz instead of the number
+// Problem
+// We need software to deliver the proper data to the scoreboard for a basketball team. Unfortunately the people using our software are not the brightest lights under the sun, so they need six buttons (each team can score either 1, 2 or 3 points with a single shot).
+
+// Your Task
+// Write a class ScoreKeeper which offers following methods:
+
+// void scoreTeamA1()
+// void scoreTeamA2()
+// void scoreTeamA3()
+// void scoreTeamB1()
+// void scoreTeamB2()
+// void scoreTeamB3()
+// String getScore()
 
 class ScoreKeeper {
+  constructor(){
+    this.teamAScore = 0
+  }
 
+  getScore(){
+    const teamA = this.stringifyScore(this.teamAScore)
+    // const teamA = stringifyScore(this.teamAScore)
+    return `${teamA}-000`;
+  }
+  scoreTeamA1(){
+    this.teamAScore += 1
+  }
+  scoreTeamA2(){
+    this.teamAScore += 2
+  }
+  scoreTeamA3(){
+    this.teamAScore += 3
+  }
+
+  stringifyScore(score){
+    if(score.toString().length === 1) return `00${score}`
+    if(score.toString().length === 2) return `0${score}`
+    return score.toString();
+  }
 }
 
 module.exports = ScoreKeeper;
--- 9_ScoreKeeper/ScoreKeeper.test.js
+++ 9_ScoreKeeper/ScoreKeeper.test.js
@@ -3,5 +3,48 @@ const ScoreKeeper = require("./ScoreKeeper");
 
 
 describe("ScoreKeeper", () => {
+  let sk;
 
+  beforeEach(()=>{
+    sk = new ScoreKeeper();
+  })
+  it('should get zero score to start', () => {
+    const score = sk.getScore()
+    expect(score).toBe('000-000')
+  });
+  it('should give A team 1 point', () => {
+    sk.scoreTeamA1()
+    const score = sk.getScore()
+    expect(score).toBe('001-000')
+  });
+  it('should give A team 2 point', () => {
+    sk.scoreTeamA2()
+    const score = sk.getScore()
+    expect(score).toBe('002-000')
+  });
+  it('should give A team 3 point', () => {
+    sk.scoreTeamA3()
+    const score = sk.getScore()
+    expect(score).toBe('003-000')
+  });
+  it('should give A team 10 point', () => {
+    sk.scoreTeamA3()
+    sk.scoreTeamA3()
+    sk.scoreTeamA3()
+    sk.scoreTeamA1()
+    const score = sk.getScore()
+    expect(score).toBe('010-000')
+  });
+  it('should stringify score 1 to 001', () => {
+    const score = sk.stringifyScore(1)
+    expect(score).toBe('001')
+  });
+  it('should stringify score 10 to 010', () => {
+    const score = sk.stringifyScore(10)
+    expect(score).toBe('010')
+  });
+  it('should stringify score 100 to 100', () => {
+    const score = sk.stringifyScore(100)
+    expect(score).toBe('100')
+  });
 });
