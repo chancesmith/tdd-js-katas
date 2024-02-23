ec38d625feee4923726116e886cf196120e1f24c
Chance Smith <chance.smith@lifeway.com>
2024-02-23 15:48:49 -0600

    📚 DOC: reset the total-calc challenge

--- src/12_refactor_calc_total/total-calc.js
+++ src/12_refactor_calc_total/total-calc.js
@@ -1,20 +1,19 @@
+// Refactor the following
+// 1. Extract the logic to calculate the total
+// 2. how can we return the total and remove the `let` and `forEach` loop
+
 function calculateTotal(cart) {
   let total = 0;
-
-  for (const item of cart) {
+  cart.forEach((item) => {
     let price = item.price;
-
     if (item.discount) {
       price *= 1 - item.discount;
     }
-
     if (item.tax) {
       price *= 1 + item.tax;
     }
-
     total += price;
-  }
-
+  });
   return total;
 }
 
