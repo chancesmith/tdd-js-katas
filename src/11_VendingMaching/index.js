787443edf69de88488fdeffbe4d31c0b5defb2ce
Chance Smith <chance.smith@lifeway.com>
2024-11-11 12:41:18 -0600

    Create index.js

---
+++ src/11_VendingMaching/index.js
@@ -0,0 +1,1 @@
+// You need to write code to implement a Vending machine that has a bunch of products like chocolates, candy, cold-drink, and accept some coins like Nickel, Dime, Quarter, Cent, etc. Make sure you insert a coin, get a product back, and get your chance back. Also, write the Unit test to demonstrate that these common use cases work.
